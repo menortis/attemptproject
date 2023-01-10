3056de8807883e84815e854beca18c2e445134bf
menortis <menortismf@gmail.com>
2023-01-10 18:11:02 +0300

    First Commit

--- updateplus.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once "connect.php";
-
-    $product_id = $_GET['product_id'];
-    
-    $db->query("UPDATE products SET PRODUCT_QUANTITY = PRODUCT_QUANTITY + 1 WHERE PRODUCT_ID = " . $product_id);
-
-    header('Location: /');
-?>
